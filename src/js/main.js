ff55e9243ea4be93eb9fc99b5ff8ad727f1606ac
MaratTukhtarov <tukhtarov.m@future-group.ru>
2020-03-16 18:12:38 +0300

    Добавил карту и точку

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,30 @@
  * Custom
  */
 //= partials/component1.js
+
+$(document).ready(function() {
+	(function() {
+		var el;
+		var cb = function(e) {
+			if (e && e.target && e.target.tagName !== "path") return;
+			if (e && e.target) {
+				el = $(e.target);
+				e.stopPropagation();
+				e.preventDefault();
+			}
+
+			var dot = $(".map__dot").css({ display: "block" });
+
+			var left = Math.floor(el.width() / 2);
+			var top = Math.floor(el.height() / 2);
+
+			var parentTop = el.position().top;
+			var parentLeft = el.position().left;
+			dot.css({ top: top + parentTop, left: left + parentLeft });
+		};
+		if ($(".map__item svg").length) {
+			$(".map__item svg").on("click", cb);
+			window.addEventListener("resize", cb());
+		}
+	})();
+});
